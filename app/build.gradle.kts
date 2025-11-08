cb78253c32c8dbf4834eeae120bbbb752991d9cf
Lev <levtokaevr@gmail.com>
2025-11-08 15:58:02 +0300

    add dependency retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,9 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
