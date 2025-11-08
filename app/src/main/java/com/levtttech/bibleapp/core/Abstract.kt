1d73e1bcb7693266448b0587a0d0e0128d30ca64
Lev <levtokaevr@gmail.com>
2025-11-08 14:10:41 +0300

    create unit test mapper

---
+++ app/src/main/java/com/levtttech/bibleapp/core/Abstract.kt
@@ -0,0 +1,13 @@
+package com.levtttech.bibleapp.core
+
+abstract class Abstract {
+
+    abstract class Object<T, M: Mapper> {
+        abstract fun map(mapper: M): T
+    }
+
+
+    interface Mapper {
+        class Empty: Mapper
+    }
+}
