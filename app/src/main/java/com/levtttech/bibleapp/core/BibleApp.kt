19905ca4c2462676008a35a00aa8f99827bab6d5
Lev <levtokaevr@gmail.com>
2025-11-12 21:04:49 +0300

    HB01 domain layer for books

--- app/src/main/java/com/levtttech/bibleapp/core/BibleApp.kt
+++ app/src/main/java/com/levtttech/bibleapp/core/BibleApp.kt
@@ -1,11 +1,16 @@
 package com.levtttech.bibleapp.core
 
 import android.app.Application
+import com.levtttech.bibleapp.data.BooksRepository
+import com.levtttech.bibleapp.domain.BaseBookDataToDomainMapper
+import com.levtttech.bibleapp.domain.BooksInteractor
 
 class BibleApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val booksRepository: BooksRepository = TODO("merge")
 
+        val booksInteractor = BooksInteractor.Base(booksRepository, BaseBookDataToDomainMapper())
     }
 }
---
+++ app/src/main/java/com/levtttech/bibleapp/domain/ErrorType.kt
@@ -0,0 +1,7 @@
+package com.levtttech.bibleapp.domain
+
+enum class ErrorType {
+    NO_CONNECTION,
+    SERVICE_UNAVAILABLE,
+    GENERIC_ERROR
+}
