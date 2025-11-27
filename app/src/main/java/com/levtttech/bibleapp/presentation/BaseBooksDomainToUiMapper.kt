07a49c0cf706f33f0d65f8a12e061998beb26a03
Lev <levtokaevr@gmail.com>
2025-11-27 21:58:53 +0300

    HB02 improved logic mappers, test, objects

--- app/src/main/java/com/levtttech/bibleapp/presentation/BaseBooksDomainToUiMapper.kt
+++ app/src/main/java/com/levtttech/bibleapp/presentation/BaseBooksDomainToUiMapper.kt
@@ -1,5 +1,6 @@
 package com.levtttech.bibleapp.presentation
 
+import com.levtttech.bibleapp.R
 import com.levtttech.bibleapp.domain.BookDomain
 import com.levtttech.bibleapp.domain.BookDomainToUiMapper
 import com.levtttech.bibleapp.domain.BooksDomainToUiMapper
@@ -7,8 +8,17 @@ import com.levtttech.bibleapp.domain.ErrorType
 
 class BaseBooksDomainToUiMapper(
     private val resourceProvider: ResourceProvider,
-    private val mapper: BookDomainToUiMapper,
+    private val bookDomainToUiMapper: BookDomainToUiMapper,
 ) : BooksDomainToUiMapper {
-    override fun map(books: List<BookDomain>): BooksUi = BooksUi.Success(books, mapper)
-    override fun map(errorType: ErrorType): BooksUi = BooksUi.Fail(errorType, resourceProvider)
+    override fun map(books: List<BookDomain>): BooksUi =
+        BooksUi.Base(books.map {it.map(bookDomainToUiMapper)})
+
+    override fun map(errorType: ErrorType): BooksUi {
+        val message = when (errorType) {
+            ErrorType.NO_CONNECTION -> resourceProvider.getString(R.string.no_connection_message)
+            ErrorType.SERVICE_UNAVAILABLE -> resourceProvider.getString(R.string.service_unavailable_message)
+            ErrorType.GENERIC_ERROR -> resourceProvider.getString(R.string.generic_error_message)
+        }
+        return BooksUi.Base(listOf(BookUi.Fail(message)))
+    }
 }
